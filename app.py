0a6c4c68474f3f390df1de86f8c11b29d3c2e57d
Davis Haupt <davis.haupt@gmail.com>
2020-09-15 12:36:00 -0400

    updated for 2020C

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+
+DB_FILE = "clubreview.db"
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_FILE}"
+db = SQLAlchemy(app)
+
+from models import *
+
+
+@app.route('/')
+def main():
+    return "Welcome to Penn Club Review!"
+
+@app.route('/api')
+def api():
+    return jsonify({"message": "Welcome to the Penn Club Review API!."})
+
+if __name__ == '__main__':
+    app.run()
