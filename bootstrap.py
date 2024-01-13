19f105afeaa117604699613d9b7041dc17a3b935
Christopher Liu <liuchris@seas.upenn.edu>
2024-01-13 14:30:47 -0500

    Reformat with Black

--- bootstrap.py
+++ bootstrap.py
@@ -1,17 +1,19 @@
 import os
 from app import db, DB_FILE
 
+
 def create_user():
     print("TODO: Create a user called josh")
 
+
 def load_data():
     from models import *
+
     print("TODO: Load in clubs.json to the database.")
 
 
-
 # No need to modify the below code.
-if __name__ == '__main__':
+if __name__ == "__main__":
     # Delete any existing database before bootstrapping a new one.
     LOCAL_DB_FILE = "instance/" + DB_FILE
     if os.path.exists(LOCAL_DB_FILE):
